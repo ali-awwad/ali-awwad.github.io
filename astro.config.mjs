8efb7975877bc096b3eef71d1361c1a294a5b08e
AliAwwad <AliAwwad1@gmail.com>
2025-04-28 01:54:39 +0400

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
-
 import react from "@astrojs/react";
-
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ali-awwad.github.io',
+  base: '/',
   vite: {
       plugins: [tailwindcss()],
   },
-
   integrations: [react()],
 });
